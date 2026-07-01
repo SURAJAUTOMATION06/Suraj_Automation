72b0d3535144e87dfff613b071f43cb931791002
suraj.pawar <surajspawar06@gmail.com>
2026-07-01 15:19:03 +0530

    2nd July 2026 - Added Java files and project updates

---
+++ Missingnumber.java
@@ -0,0 +1,12 @@
+public class Missingnumber {
+    public static void main(String[] args){
+        int[] array = {1,3,4,5,7}
+                int i;
+        for (i = 0; i<=array.length; i++ )
+        {
+            if (i < array.length)
+            System.out.println("Missing Element are:-" + i);
+        }
+    }
+}
+
